1013ab53bf30046e38330b8f5608fb4dc28ce9bf
LucasStarlingdePaulaSalles <lucastarling1@gmail.com>
2020-10-06 19:13:04 -0300

    Commit do roteiro 2

---
+++ TDDProject/src/test/java/MoneyTest.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author lucas
+ */
+
+class Money  {
+   protected int amount;
+   
+   public boolean equals(Object object)  {
+      Money money = (Money) object;
+      return amount == money.amount;
+   }   
+}
+
+class Dollar extends Money {
+   Dollar(int amount) {
+      this.amount= amount;
+   }
+   Dollar times(int multiplier) {
+      return new Dollar(amount * multiplier);
+   }
+}
+
+class Franc extends Money {   
+   Franc(int amount) {      
+      this.amount= amount;
+    }					
+    Franc times(int multiplier)  {      
+       return new Franc(amount * multiplier);					
+    }   
+}
+
+public class MoneyTest {
+    
+    public void MoneyTest() {
+    }
+    
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
+    
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+    
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+    }
+}
